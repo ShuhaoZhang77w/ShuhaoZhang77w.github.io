8bee1a8c9a8e4df841cb94d390325422c81f8d8d
张书豪 <zhangshuhao@Mac-mini.local>
2024-10-08 20:27:56 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const greetings = ["Hi", "你好", "Salut", "Hola", "Ciao", "Hallo", "Hej"];
+    let index = 0;
+
+    function changeGreeting() {
+        const greetingElement = document.getElementById("greeting");
+
+        greetingElement.style.opacity = 0;
+
+        setTimeout(() => {
+            greetingElement.textContent = greetings[index];
+            greetingElement.style.opacity = 1;
+            index = (index + 1) % greetings.length;
+        }, 1000);
+    }
+
+    setInterval(changeGreeting, 3000);
+
+    changeGreeting();
+});
